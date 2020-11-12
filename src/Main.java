59c08b4e0e5e51ca4a1a16ba300fd3c0525fe702
Mark G <mark.garcia.8001@gmail.com>
2020-11-11 17:20:57 -0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -6,6 +5,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args){
         readInput("input.txt");
+        System.out.println(mazes.size());
+//        System.out.println(temp[0][0].n + "" + temp[0][0].s + temp[0][0].w + temp[0][0].e);
+        System.out.println(dfs(mazes.get(0)));
+        System.out.println(dfs(mazes.get(1)));
+        System.out.println(dfs(mazes.get(2)));
+        System.out.println(dfs(mazes.get(3)));
 
 
 
@@ -15,7 +20,6 @@ public class Main {
 
     public static class Cell{
         public int n, s, w, e;
-        public int timesExplored;
         public int numN, numS, numW, numE;
 
         public Cell(int n, int s, int w, int e){
@@ -27,6 +31,13 @@ public class Main {
             numS = 0;
             numW = 0;
             numE = 0;
+        }
+
+        public void closeCell(){
+            this.n = 1;
+            this.s = 1;
+            this.w = 1;
+            this.e = 1;
         }
 
 
@@ -54,7 +65,8 @@ public class Main {
                         int s = Integer.valueOf(temp[1]);
                         int w = Integer.valueOf(temp[2]);
                         int e = Integer.valueOf(temp[3]);
-                        tempMaze[i][j / 4] = new Cell(n, s, e, w);
+                        //System.out.println("" + n + s + w + e);
+                        tempMaze[j][k / 4] = new Cell(n, s, w, e);
                     }
                 }
                 mazes.add(tempMaze);
@@ -85,14 +97,127 @@ public class Main {
             row++;
             current = maze[row][col];
         }
-
         return dfs(maze, start, path, current, row, col);
     }
 
     private static String dfs(Cell[][] m, Cell start, String p, Cell current, int r, int c){
-        //base case: Are we already at the start?
+        //base case: Are we at the start?
         if(current == start){
             return p;
+        } else {
+            //not at start check prev direction
+            char prevDirection = p.charAt(p.length() - 1);
+
+            switch (prevDirection) {
+                case 'S':
+                    if (current.s == 0) { // another south opening
+                        p += "S";
+                        current.numS += 1;
+                        r++;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else if(current.w == 0){ // a west opening
+                        p += "W";
+                        current.numW += 1;
+                        c--;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else if(current.e == 0) { // an east opening
+                        p += "E";
+                        current.numE += 1;
+                        c++;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else { // no other opening go back north
+                        //close the cell since we do not want to get back into this one
+                        m[r][c].closeCell();
+                        p += "N";
+                        current.numN += 1;
+                        r--;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    }
+                case 'N':
+                    if (current.w == 0) { // Check west opening first since going south doesnt make sense
+                        p += "W";
+                        current.numW += 1;
+                        c--;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else if(current.e == 0){ // a east opening
+                        p += "E";
+                        current.numW += 1;
+                        c++;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else if(current.n == 0) { // an north opening
+                        p += "N";
+                        current.numN += 1;
+                        r--;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else { // no other opening go back south
+                        m[r][c].closeCell();
+                        p += "S";
+                        current.numS += 1;
+                        r++;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    }
+                case 'E':
+                    if(current.n == 0){
+                        p += "N";
+                        current.numN += 1;
+                        r--;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else if (current.s == 0){
+                        p += "S";
+                        current.numS += 1;
+                        r++;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else if (current.e == 0){
+                        p += "E";
+                        current.numW += 1;
+                        c++;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else { //no other opening go back west
+                        m[r][c].closeCell();
+                        p += "W";
+                        c--;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    }
+                case 'W':
+                    if(current.n == 0){
+                        p += "N";
+                        current.numN += 1;
+                        r--;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else if (current.s == 0){
+                        p += "S";
+                        current.numS += 1;
+                        r++;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else if (current.w == 0) {
+                        p += "W";
+                        current.numW += 1;
+                        c--;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    } else { // no other opening go back east
+                        m[r][c].closeCell();
+                        p += "E";
+                        c++;
+                        current = m[r][c];
+                        return dfs(m, start, p, current, r, c);
+                    }
+
+            }
         }
 
         return p;
